8e470d78f62cd86f8119bd7b086ff30526d19821
Daniel Finch <xiphoid0311@gmail.com>
2023-06-05 22:41:42 -0500

    creating an array and lists display odds and evens

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
 
 namespace ArraysAndLists
 {
@@ -7,25 +9,49 @@ namespace ArraysAndLists
         static void Main(string[] args)
         {
             //TODO:
-
+                                                                        //done
             // Create an int Array and populate numbers 1-10
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+
 
             /* Create two Lists of type int.
-             * Name one List "evens"
+             * Name one List "evens"                                    //done
              * Name the other List "odds"
              */
+            var evens = new List<int>();
 
+            var odds = new List<int>();
+
+            foreach (int i in numbers)
+            {
+                
+
+                if (i % 2 == 0)
+                { evens.Add(i); }
+                else { odds.Add(i); }
+            }
+            
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
-             *  if a number is even or odd.
+             *  if a number is even or odd.                             //done
              * Then add those numbers to either the evens List
              * or the odds List
              */
+            foreach (int i in odds)
+            {
+                Console.WriteLine(i);
+            }
 
+            foreach (int i in evens)
+            {
+                Console.Write(i);
+            }
             /* Now using foreach or for loops,
              * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
+             *                                                          //done
+             * Try to be creative in your display       //unsure?
              */
         }
     }
